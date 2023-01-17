2bbf63598f70d132356fa1effc27a6b41536901d
Aubrey Jones <aubrey_jones07@hotmail.com>
2023-01-17 01:30:31 -0700

    Replaced Bootstrap with MUI5 and started fetch fun

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,4 @@
 import React from 'react'; 
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-
 
 
 
@@ -13,15 +10,8 @@ import Navbar from 'react-bootstrap/Navbar';
 const Footer = () => {
   return (
     <>
-    <Container>
-       <Navbar expand="lg" variant="light" bg="light">
-         <Container>
-           <Navbar.Brand href="#">
-           <p>Aubrey Jones Creatives Copyright &copy; 2023 Aubrey Jones</p>
-           </Navbar.Brand>
-         </Container>
-       </Navbar>
-     </Container>
+    <p>Aubrey Jones Creatives Copyright &copy; 2023 Aubrey Jones</p>
+    
    </>
   )
 }
--- src/pages/Weather.js
+++ src/pages/Weather.js
@@ -1,5 +1,97 @@
-import React from 'react'; 
-import Nav from 'react-bootstrap/Nav';
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+
+
+
+
+const searchHeader = {
+marginLeft: '50px', 
+padding: '20px'
+
+
+
+}
+
+
+
+
+const Search = styled('div')(({ theme }) => ({
+    display: 'inline-flex', 
+    margin: '10px', 
+    width: '25%'
+
+
+  }));
+  
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    fontSize: '20px', 
+    backgroundColor: 'blue', 
+    width: '35px', 
+    textAlign: 'center', 
+    color: 'white', 
+    borderRadius: '0px 25px 25px 0px', 
+    cursor: 'pointer', 
+    height: '32px'
+
+  }));
+  
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+   border: '1px solid gray', 
+   borderRadius: '25px 0px 0px 25px', 
+   padding: '15px', 
+   height: '15px'
+
+    
+  }));
+
+
+
+// fetch to openweathermap API 
+// sourced from MDN's Fetch function 
+
+
+
+
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
+  
+  postData('https://example.com/answer', { answer: 42 })
+    .then((data) => {
+      console.log(data); // JSON data parsed by `data.json()` call
+    });
+
+
+
+
+
+
+
+
+
 
 
 
@@ -7,15 +99,24 @@ import Nav from 'react-bootstrap/Nav';
 
 const Weather = () => {
   return (
+<>
 
-<Nav defaultActiveKey="/home" className="flex-column">
-      <Nav.Link href="/home">Active</Nav.Link>
-      <Nav.Link eventKey="link-1">Link</Nav.Link>
-      <Nav.Link eventKey="link-2">Link</Nav.Link>
-      <Nav.Link eventKey="disabled" disabled>
-        Disabled
-      </Nav.Link>
-    </Nav>
+<h3 sx={searchHeader}>Search a City</h3>
+
+
+    <Search>
+          
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+              <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+          </Search>
+
+
+    </>
   )
 }
 
